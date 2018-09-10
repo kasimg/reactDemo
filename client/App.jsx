3497c08d86d6e210f876df62155eea2766216d7b
kasim <kasim@kasimdeMacBook-Air.local>
2018-09-10 14:30:33 +0800

    first commit

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default () => <div>This is app 1234599</div>
+export default () => <div>This is app 1234599</div>;
 
 
 // export default class App extends React.Component {
